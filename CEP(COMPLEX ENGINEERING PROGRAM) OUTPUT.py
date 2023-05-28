643e8dc0877f65592264475d0dcc84b81e9bd090
SamreenRazzaq <96229701+SamreenRazzaq@users.noreply.github.com>
2023-05-28 18:50:28 +0500

    Add files via upload

---
+++ CEP(COMPLEX ENGINEERING PROGRAM) OUTPUT.py
@@ -0,0 +1,21 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 16:33:24) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/DELL/Desktop/CEP(COMPLEX ENGINEERING PROGRAM)2INPUT.py ===
+Consider mouse is at zero position
+Enter position of catA: 5
+Enter position of catB: 10
+catA
+>>> 
+=== RESTART: C:/Users/DELL/Desktop/CEP(COMPLEX ENGINEERING PROGRAM)2INPUT.py ===
+Consider mouse is at zero position
+Enter position of catA: 0
+Enter position of catB: 0
+Program ends
+>>> 
+=== RESTART: C:/Users/DELL/Desktop/CEP(COMPLEX ENGINEERING PROGRAM)2INPUT.py ===
+Consider mouse is at zero position
+Enter position of catA: 5
+Enter position of catB: 5
+Mouse escape as two cats fought
+>>> 
